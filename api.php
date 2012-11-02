64dc3a3bd5241851cc10e53ee282b1109d21ab37
Kartik Mandaville <kar2905@gmail.com>
2012-11-02 19:29:25 -0400

    dump of data

--- api.php
+++ api.php
@@ -1,5 +1,6 @@
 <?php
 require("connect_db.php");
+//header("Content-type: text/xml");
 
 $type = $_GET['type'];
 $lat = $_GET['lat'];
@@ -7,8 +8,11 @@ $lng = $_GET['lng'];
 
 
 $query = "SELECT ID, ( 3959 * acos( cos( radians($lat) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians($lng) ) + sin( radians($lat) ) * sin( radians( lat ) ) ) ) AS distance FROM data HAVING distance < 10 ORDER BY distance";
+echo $query;
 $result = mysql_query($query) or die(mysql_error().$query);
 $rows = mysql_fetch_assoc($result);
+echo $result;
+var_dump($rows);
 foreach($rows as $row){
   print_r($row);
   
