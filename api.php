3504576794871cb06f36cf499940e4f1bc454db1
Kartik Mandaville <kar2905@gmail.com>
2012-11-02 20:15:25 -0400

    db search

--- api.php
+++ api.php
@@ -7,14 +7,16 @@ $lat = $_GET['lat'];
 $lng = $_GET['lng'];
 
 
-$query = "SELECT ID, ( 3959 * acos( cos( radians($lat) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians($lng) ) + sin( radians($lat) ) * sin( radians( lat ) ) ) ) AS distance FROM data HAVING distance < 10 ORDER BY distance";
-echo $query;
+$query = "SELECT ID,Location,StartTime,EndTime, ( 3959 * acos( cos( radians($lat) ) * cos( radians( Lat ) ) * cos( radians( Lng ) - radians($lng) ) + sin( radians($lat) ) * sin( radians( Lat ) ) ) ) AS distance FROM data HAVING distance < 10 ORDER BY distance";
+//echo $query;
 $result = mysql_query($query) or die(mysql_error().$query);
-$rows = mysql_fetch_assoc($result);
-echo $result;
-var_dump($rows);
-foreach($rows as $row){
+//$rows = mysql_fetch_assoc($result);
+//echo $result;
+//var_dump($rows);
+while($row=mysql_fetch_assoc($result)){
+  //echo "ad";
   print_r($row);
+  echo "<br/>";
   
 }
 
