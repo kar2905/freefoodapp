bc5f16bf78d403e411f07b67bca19260fe9cbb76
Kartik Mandaville <kar2905@gmail.com>
2012-11-03 03:57:46 -0400

    adding server config

--- connect_db.php
+++ connect_db.php
@@ -4,4 +4,6 @@
 
 $connect = mysql_connect('localhost','root','root') or die(mysql_error());
 mysql_select_db('freefoodapp',$connect);
+$connect = mysql_connect('localhost','sachinprabhu_ffp','f5daa32b') or die(mysql_error());
+mysql_select_db('sachinprabhu_ffp',$connect);
 ?>
